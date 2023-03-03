223d0e74c5da2475635ca500170aa95f7c4d5e22
Ariq Naufal <ariq.naufal29@gmail.com>
2023-03-03 21:46:51 +0700

    Session 7 - SSH & FTP

--- session-7/main.go
+++ session-7/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"io/ioutil"
+	"io"
 	"log"
+	"os"
+
+	"github.com/pkg/sftp"
 
 	"golang.org/x/crypto/ssh"
 )
@@ -24,15 +27,16 @@ func main() {
 
 	// Otentikasi menggunakan identity file
 
-	const SSH_ADDRESS = "192.168.0.24:22"
-	const SSH_USERNAME = "user"
-	const SSH_KEY = "path/to/file/identity.pem"
+	const SSH_ADDRESS = "0.0.0.0:22"
+	const SSH_USERNAME = "testsftp"
+	const SSH_PASSWORD = "password"
+	// const SSH_KEY = "path/to/file/identity.pem"
 
 	sshConfig := &ssh.ClientConfig{
 		User:            SSH_USERNAME,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 		Auth: []ssh.AuthMethod{
-			PublicKeyFile(SSH_KEY),
+			ssh.Password(SSH_PASSWORD),
 		},
 	}
 
@@ -44,7 +48,71 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to dial. " + err.Error())
 	}
+
+	// create session
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session. " + err.Error())
+	}
+	//session.Stdin = os.Stdin
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+
+	// test session
+	stdinBuf, _ := session.StdinPipe()
+	if err := session.Shell(); err != nil {
+		panic(err)
+	}
+	stdinBuf.Write([]byte("echo hello\n"))
+	stdinBuf.Write([]byte("ls -l ~/\n"))
+	// err = session.Run("ls -l ~/")
+	// if err != nil {
+	// 	log.Fatal("Command execution error. " + err.Error())
+	// }
+
+	// test sftp
+	sftpClient, err := sftp.NewClient(client)
+	if err != nil {
+		log.Fatal("Failed create client sftp client. " + err.Error())
+	}
+
+	//err = session.Run("touch ~/test-file.txt")
+	//if err != nil {
+	//	log.Fatal("Command execution error. " + err.Error())
+	//}test_dir
+	//session.Close()
+
+	/*
+		// create file with os library, change permissions and write text in file
+			dir := "test_dir"
+			os.Mkdir(dir, 0777)
+			fileName := path.Join(dir, "test-file.txt")
+
+			ioutil.WriteFile(fileName, []byte("test"), 0666)
+	*/
+	// D:/Multipolar-Projects/Training Golang Intermediate Programming/GLIM_Hacktiv8/golang-intermediate/session-7/test_dir/
+
+	fDestination, err := sftpClient.Create("test-file.txt")
+	if err != nil {
+		log.Fatal("Failed to create destination file. " + err.Error())
+	}
+
+	fSource, err := os.Open("test-file.txt")
+	if err != nil {
+		log.Fatal("Failed to read source file. " + err.Error())
+	}
+
+	_, err = io.Copy(fDestination, fSource)
+	if err != nil {
+		log.Fatal("Failed copy source file into destination file. " + err.Error())
+	}
+
+	log.Println("File copied.")
+
+	// create new session
 }
+
+/*
 
 func PublicKeyFile(file string) ssh.AuthMethod {
 	buffer, err := ioutil.ReadFile(file)
@@ -59,3 +127,5 @@ func PublicKeyFile(file string) ssh.AuthMethod {
 
 	return ssh.PublicKeys(key)
 }
+
+*/
